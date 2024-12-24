c0831bc283539adf1d96fdbb6d146f58e62a500f
Ky0sann <mathis.huard@supdevinci-edu.fr>
2024-12-24 15:42:41 +0100

    Custom Items and Creative Mode Tab

--- src/main/java/net/mathis/tutorialmod/TutorialMod.java
+++ src/main/java/net/mathis/tutorialmod/TutorialMod.java
@@ -1,6 +1,9 @@
 package net.mathis.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.mathis.tutorialmod.item.ModCreativeModTabs;
+import net.mathis.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -24,6 +27,9 @@ public class TutorialMod {
     public TutorialMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModCreativeModTabs.register(modEventBus);
+        ModItems.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
         modEventBus.addListener(this::addCreative);
 
@@ -36,7 +42,10 @@ public class TutorialMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        // Add creative mode logic
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.SAPPHIRE);
+            event.accept(ModItems.RAW_SAPPHIRE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
