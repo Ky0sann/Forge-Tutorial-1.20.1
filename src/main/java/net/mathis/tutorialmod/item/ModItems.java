59de0f8a143e9cf76547f88866b2851bf51f861a
Ky0sann <mathis.huard@supdevinci-edu.fr>
2024-12-31 12:10:51 +0100

    Fuel Items (Pine Cone)

--- src/main/java/net/mathis/tutorialmod/item/ModItems.java
+++ src/main/java/net/mathis/tutorialmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.mathis.tutorialmod.item;
 
 import net.mathis.tutorialmod.TutorialMod;
+import net.mathis.tutorialmod.item.custom.FuelItem;
 import net.mathis.tutorialmod.item.custom.MetalDetectorItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -23,6 +24,9 @@ public class ModItems {
 
     public static final RegistryObject<Item> STRAWBERRY = ITEMS.register("strawberry",
             () -> new Item(new Item.Properties().food(ModFoods.STRAWBERRY)));
+
+    public static final RegistryObject<Item> PINE_CONE = ITEMS.register("pine_cone",
+            () -> new FuelItem(new Item.Properties(), 400));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
