d9ad0aaa0e937d5c230d71455f4f4e44c7d23370
Ky0sann <mathis.huard@supdevinci-edu.fr>
2025-01-01 12:05:44 +0100

    Custom Tags

--- src/main/java/net/mathis/tutorialmod/item/custom/MetalDetectorItem.java
+++ src/main/java/net/mathis/tutorialmod/item/custom/MetalDetectorItem.java
@@ -1,5 +1,6 @@
 package net.mathis.tutorialmod.item.custom;
 
+import net.mathis.tutorialmod.util.ModTags;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
@@ -62,6 +63,6 @@ public class MetalDetectorItem extends Item {
     }
 
     private boolean isValuableBlock(BlockState state) {
-            return state.is(Blocks.IRON_ORE) || state.is(Blocks.DIAMOND_ORE);
+            return state.is(ModTags.Blocks.METAL_DETECTOR_VALUABLES);
         }
     }
