a6f2670984bda2d22dd74db7914eee40737a7745
Ky0sann <mathis.huard@supdevinci-edu.fr>
2025-01-08 09:53:09 +0100

    Full Armor Effect

--- src/main/java/net/mathis/tutorialmod/item/ModItems.java
+++ src/main/java/net/mathis/tutorialmod/item/ModItems.java
@@ -3,6 +3,7 @@ package net.mathis.tutorialmod.item;
 import net.mathis.tutorialmod.TutorialMod;
 import net.mathis.tutorialmod.item.custom.FuelItem;
 import net.mathis.tutorialmod.item.custom.MetalDetectorItem;
+import net.mathis.tutorialmod.item.custom.ModArmorItem;
 import net.minecraft.world.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -47,7 +48,7 @@ public class ModItems {
             () -> new HoeItem(ModToolTiers.SAPPHIRE, 0, 0, new Item.Properties()));
 
     public static final RegistryObject<Item> SAPPHIRE_HELMET = ITEMS.register("sapphire_helmet",
-            () -> new ArmorItem(ModArmorMaterials.SAPPHIRE, ArmorItem.Type.HELMET, new Item.Properties()));
+            () -> new ModArmorItem(ModArmorMaterials.SAPPHIRE, ArmorItem.Type.HELMET, new Item.Properties()));
     public static final RegistryObject<Item> SAPPHIRE_CHESTPLATE = ITEMS.register("sapphire_chestplate",
             () -> new ArmorItem(ModArmorMaterials.SAPPHIRE, ArmorItem.Type.CHESTPLATE, new Item.Properties()));
     public static final RegistryObject<Item> SAPPHIRE_LEGGINGS = ITEMS.register("sapphire_leggings",
