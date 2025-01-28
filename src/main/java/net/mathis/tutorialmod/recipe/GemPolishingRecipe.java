a2d91b2ae3352f6d6fce773b22e3170601714af7
Ky0sann <mathis.huard@supdevinci-edu.fr>
2025-01-28 18:45:27 +0100

    Bonus : JEI Compatibility (for user-friendly showing recipes)

--- src/main/java/net/mathis/tutorialmod/recipe/GemPolishingRecipe.java
+++ src/main/java/net/mathis/tutorialmod/recipe/GemPolishingRecipe.java
@@ -34,6 +34,11 @@ public class GemPolishingRecipe implements Recipe<SimpleContainer> {
     }
 
     @Override
+    public NonNullList<Ingredient> getIngredients() {
+        return inputItems;
+    }
+
+    @Override
     public ItemStack assemble(SimpleContainer simpleContainer, RegistryAccess registryAccess) {
         return output.copy();
     }
