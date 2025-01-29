7fc736d850eccf8204a394ef71d3e2bf22da68e7
Ky0sann <mathis.huard@supdevinci-edu.fr>
2025-01-29 18:52:03 +0100

    Block Entity Renderers

--- src/main/java/net/mathis/tutorialmod/event/ModEventBusClientEvents.java
+++ src/main/java/net/mathis/tutorialmod/event/ModEventBusClientEvents.java
@@ -1,6 +1,8 @@
 package net.mathis.tutorialmod.event;
 
 import net.mathis.tutorialmod.TutorialMod;
+import net.mathis.tutorialmod.block.entity.ModBlockEntities;
+import net.mathis.tutorialmod.block.entity.renderer.GemPolishingBlockEntityRenderer;
 import net.mathis.tutorialmod.entity.client.ModModelLayers;
 import net.mathis.tutorialmod.entity.client.RhinoModel;
 import net.minecraftforge.api.distmarker.Dist;
@@ -13,5 +15,10 @@ public class ModEventBusClientEvents {
     @SubscribeEvent
     public static void registerLayer(EntityRenderersEvent.RegisterLayerDefinitions event) {
         event.registerLayerDefinition(ModModelLayers.RHINO_LAYER, RhinoModel::createBodyLayer);
+    }
+
+    @SubscribeEvent
+    public static void registerBER(EntityRenderersEvent.RegisterRenderers event) {
+        event.registerBlockEntityRenderer(ModBlockEntities.GEM_POLISHING_BE.get(), GemPolishingBlockEntityRenderer::new);
     }
 }
